303ee4ef39983d4588074668eb4891404e5a02e2
Pei-Ya Chiu <lockercho@gmail.com>
2011-12-21 03:21:09 +0800

    bull shit

---
+++ src/proj/tool/MyAdapter.java
@@ -0,0 +1,40 @@
+package proj.tool;
+
+import com.cyrilmottier.android.greendroid.R;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+public class MyAdapter extends CursorAdapter {
+	private LayoutInflater inflater;
+	public MyAdapter(Context context, Cursor c) {
+		super(context, c);
+		inflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public void bindView(View v, Context context, Cursor c) {
+		String latitude = c.getString(c.getColumnIndex("latitude"));
+		String longitude = c.getString(c.getColumnIndex("longitude"));
+		String timeval = c.getString(c.getColumnIndex("timeval"));
+		String unixTime = c.getString(c.getColumnIndex("unixTime"));
+			
+		TextView gpsInfo = (TextView) v.findViewById(R.id.gpsInfo);
+		TextView timeInfo = (TextView) v.findViewById(R.id.timeInfo);
+			
+		gpsInfo.setText("lat: "+latitude+", long: "+longitude);
+		timeInfo.setText("unix: "+unixTime+", val:"+timeval);	
+
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		View v = inflater.inflate( R.layout.tmplistviewitem, parent, false);
+		return v;
+	}
+
+}
--- src/proj/tool/TaxiData.java
+++ src/proj/tool/TaxiData.java
@@ -1,5 +1,8 @@
 package proj.tool;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 
@@ -9,11 +12,32 @@ import android.content.Context;
 
 public class TaxiData {
 	Context context;
+	String str;
 	TaxiData(Context c){
 		context = c;
-		InputStream is = c.getResources().openRawResource(R.raw.taxilist);	
-		
+		BufferedInputStream bis = new BufferedInputStream(c.getResources().openRawResource(R.raw.taxilist));
+		DataInputStream dis = new DataInputStream(bis);
+		int i = 0;
+		try {
+			while((str = dis.readLine()) != null){
+				switch(i){
+					case 0:
+						break;
+					case 1:
+						break;
+					case 2:
+						break;
+					default:
+						break;	
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 	
 	public HashMap<String,String> getDataByLocation(){
 		return null;
